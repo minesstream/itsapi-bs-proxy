88fa152d080fc7832fb0767ced94e1766c657e65
gookcompsci <gookcompsci@hotmail.com>
2023-08-28 23:26:14 +0700

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios').default;
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+const cfg = require('./config.json')
+
+let its_result = {'Result':true}
+
+async function proxyPost(url,data) {
+    try {
+      await axios.post(url, data);
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+app.post('/NotificationInfo/TollgateInfo', (req, res) => {
+    proxyPost(cfg.BS_TOLL_PATH,req.body)
+    res.json(its_result)
+})
+
+app.post('/NotificationInfo/KeepAlive', (req, res) => {
+    proxyPost(cfg.BS_HB_PATH,req.body)
+    res.json(its_result)
+})
+
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(cfg.PORT, () => {
+  console.log('Start proxy server')
+})
